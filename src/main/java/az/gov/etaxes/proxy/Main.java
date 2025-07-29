beeb170993216c87d18a5effc724b30af42490be
gunay.abbasova <gunay.abbasova@cybernet.az>
2025-07-29 18:34:19 +0400

    Set-Tapsiriq 5

--- src/main/java/az/gov/etaxes/proxy/Main.java
+++ src/main/java/az/gov/etaxes/proxy/Main.java
@@ -6,7 +6,6 @@ public class Main {
     Vehicle v = new Bicycle();
     v.move();
 
-
     MessageSender email = new EmailSender();
     MessageSender sms = new SMSSender();
 
@@ -19,30 +18,36 @@ public class Main {
     Employee e2 = new Manager();
     System.out.println("Manager maaşı: " + e2.calculateSalary());
 
-    Vehicle v1=new Car();
+    Vehicle v1 = new Car();
     v1.start();
 
     Teacher teacher = new Teacher();
     teacher.setName("Gunay");
     teacher.setSurname("Abbasova");
     teacher.setSalary(250);
-    System.out.println( teacher.getName() + " " + teacher.getSurname());
+    System.out.println(teacher.getName() + " " + teacher.getSurname());
     System.out.println("Maas: " + teacher.getSalary());
 
     Manager manager = new Manager();
-     Dog d= new Dog();
-     d.makeSound();
+    Dog d = new Dog();
+    d.makeSound();
     SavingsAccount acc = new SavingsAccount();
     acc.setBalance(1000);
     System.out.println("Faiz: " + acc.calculateInterest());
-    Students s =new Students();
-    double ortabal=s.ortaBalHesabla();
-    System.out.println("Telebenin ortalama bali: "+ortabal);
+    Students s = new Students();
+    double ortabal = s.ortaBalHesabla();
+    System.out.println("Telebenin ortalama bali: " + ortabal);
     Yemekler y = new Yemekler();
     y.yemekCesidleri();
-    Stok st =new Stok();
+    Stok st = new Stok();
     st.stokStatus();
     st.kontakNumber();
 
+    try {
+      String reng = "qirmizi";
+      int rengdenReqeme = Integer.parseInt((String) reng);
+    }catch (Exception e){
+      System.out.println("Yalnız rəqəm daxil olunmalıdır");
     }
+  }
   }
