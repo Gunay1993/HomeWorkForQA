9411f0009768af1a9775d93d221a3624a1927990
gunay.abbasova <gunay.abbasova@cybernet.az>
2025-08-02 14:06:52 +0400

    JUnit EX-4

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -12,7 +15,7 @@ public class BankAccountTest {
   BankAccount account;
   @BeforeAll
   public static void start() {
-    System.out.println("Bank sistemi başladı");
+    System.out.println("Bank sistemi başladı-beferoall");
   }
 
   @Test
@@ -25,7 +28,7 @@ public class BankAccountTest {
  @DisplayName("her test islemezden evvel yeni obyekt yaradir")
   public void crateobyekt() {
  account =new BankAccount("12345", "Əli Məmmədov");
-
+System.out.println("BeforeEach-her testden evvel metod yaradir");
   }
 @Test
 @DisplayName("balans emeliyyati")
@@ -35,13 +38,7 @@ account.withdraw(50);
 assertEquals(150, account.getBalance());
   }
 
-  @AfterEach
-  @DisplayName("her testden sora obyekti null edir")
-  public void nullObyekt() {
-   account=null;
-    System.out.println("Test tamamlandı-obykti null edir (after each)");
 
-  }
   @Test
   @DisplayName("Balansin 0 oldugunu yoxluyur")
   public  void testGetBalance(){
@@ -58,6 +55,21 @@ assertEquals(150, account.getBalance());
   public void testOwnerName(){
     assertEquals("Əli Məmmədov" , account.getOwnerName());
   }
+  @ParameterizedTest
+  @ValueSource(doubles = {10.0, 50.0, 100.0, 500.0, 1000.0})
+  @DisplayName("Müxtǝlif mǝblǝğlǝrlǝ pul yatırma testi")
+  public  void depositCheck(double amount){
+    account.deposit(amount);
+    assertEquals(amount, account.getBalance());
+  }
+
+  @AfterEach
+  @DisplayName("her testden sora obyekti null edir")
+  public void nullObyekt() {
+    account=null;
+    System.out.println("Test tamamlandı-obykti null edir (after each)");
+
+  }
 
 
   @AfterAll
@@ -65,5 +77,8 @@ assertEquals(150, account.getBalance());
   public static void close() {
     System.out.println("Bank sistemi bitdi-afterAll");
   }
+
+
+
 
 }
